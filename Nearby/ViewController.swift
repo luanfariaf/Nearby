5aff2ccbb933911992c679366b854f8e897d597e
Luan Faria <luuan.fariaf@gmail.com>
2024-12-09 20:07:49 -0300

    Initial Commit

---
+++ Nearby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Luan Faria on 09/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
